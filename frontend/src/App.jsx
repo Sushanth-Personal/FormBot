2b5db4738564ad7161c015174eb6df71b8241c39
Sushanth-Personal <sushanthp.calicut@gmail.com>
2024-12-20 05:51:31 +0530

    Setup: Basic setup Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+  } from "react-router-dom";
+  
+    function App() {
+    return (
+      <Router>
+        <Routes>
+          {/* Define routes for the login and home pages */}
+          {/* <Route path="/login" element={<LoginPage />} /> */}
+       
+         
+        </Routes>
+      </Router>
+    );
+  }
+  
+  export default App;
+  
