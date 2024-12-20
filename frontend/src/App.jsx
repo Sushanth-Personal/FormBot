259774ef45b92b3ff69fd3c5e69e87f4fc48e63e
Sushanth-Personal <sushanthp.calicut@gmail.com>
2024-12-20 22:00:26 +0530

    Add: Route added Modify: modified login page design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,23 @@
 import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-  } from "react-router-dom";
-  
-  import LandingPage from "./pages/LandingPage/LandingPage";
-  import LoginPage from "./pages/LoginPage/LoginPage";
-    function App() {
-    return (
-      <Router>
-        <Routes>
-          {/* Define routes for the login and home pages */}
-          <Route path="/" element={<LandingPage/>} />
-          <Route path="/login/" element={<LoginPage />} />
-         
-        </Routes>
-      </Router>
-    );
-  }
-  
-  export default App;
-  
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import LandingPage from "./pages/LandingPage/LandingPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Define routes for the login and home pages */}
+        <Route path="/" element={<LandingPage />} />
+
+        <Route path="/login/" element={<LoginPage />} />
+        <Route path="*" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
