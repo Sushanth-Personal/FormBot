bf731cec1afa230f169df4983fa0dcaef6ff1467
Sushanth-Personal <sushanthp.calicut@gmail.com>
2024-12-22 00:03:47 +0530

    Reset: Resetted

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import vercel from 'vite-plugin-vercel';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),vercel()],
+  plugins: [react()],
   server: {
     port: process.env.PORT || 4173, // Bind to the Render-provided port, fallback to 4173
     host: true, // Expose the app to external IPs
