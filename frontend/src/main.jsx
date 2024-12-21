756d4a6a9547a99a4184d06ebe6d56eaa48c5ed2
Sushanth-Personal <sushanthp.calicut@gmail.com>
2024-12-21 22:20:10 +0530

    Remove: Removed strict mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './main.css'
 import { UserProvider } from "./Contexts/UserContext";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
   <UserProvider>
     <App />
   </UserProvider>
-</StrictMode>, 
+
 )
 
 
